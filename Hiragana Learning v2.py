3f22c057c2b34f7a79ad5bffd643e43a20d27030
Weiqiiing <38157148+Weiqiiing@users.noreply.github.com>
2018-08-10 09:07:16 +0100

    Update and rename Hiragana learning.py to Hiragana Learning v2.py

--- Hiragana Learning v2.py
+++ Hiragana Learning v2.py
@@ -1,15 +1,25 @@
 import random
 length = 5 #Change this for second 'program' length
-jap = [ "あ","い","う","え","お",                #∅
+jap = [
+#Basic monographs
+
+            "あ","い","う","え","お",                #∅
             "か","き","く","け","こ",                #K
             "さ","し","す","せ","そ",                #S
             "た","ち","つ","て","と",                #T
             "な","に","ぬ","ね","の",                #N
             "は","ひ","ふ","へ","ほ",                #H
-            "ま","み","む","め","も"                 #M
-            "や","ゆ","よ"                                 #Y
-            "ら","り","る","れ","ろ"                 #R
-            "わ","ゐ","ゑ","を"                         #W
+            "ま","み","む","め","も" ,                #M
+            "や","ゆ","よ",                                 #Y
+            "ら","り","る","れ","ろ",                 #R
+            "わ","ゐ","ゑ","を",                         #W
+
+            #Diacritics (Dakuten)
+            "が","ぎ","ぐ","げ","ご",
+            "ざ","じ","ず","ぜ","ぞ",
+            "だ","ぢ","づ","で","ど",
+            "ば","び","ぶ","べ","ぼ",
+            "ぱ","ぴ","ぷ","ぺ","ぽ"
                                                        ]
 #When choosing what hiragana you want to use, just add a # to the beginning of each line you want to disable.
 #For example, to disable the M,Y and R Monographs, you would change the start of the lines to 
@@ -17,7 +27,11 @@ jap = [ "あ","い","う","え","お",                #∅
 #            "や","ゆ","よ"                                 #Y
 #            "ら","り","る","れ","ろ"                 #R
 #            "わ","ゐ","ゑ","を"                         #W
-eng = [ "a","i","u","e","o",
+
+
+eng = [
+             #Basic Romanji
+             "a","i","u","e","o",
              "ka","ki","ku","ke","ko",
              "sa","shi","su","se","so",
              "ta","chi","tsu","te","to",
@@ -26,10 +40,19 @@ eng = [ "a","i","u","e","o",
              "ma","mi","mu","me","mo",
              "ya","yu","yo",
              "ra","ri","ru","re","ro",
-             "wa","wi","we","wo"
-                                                    ]
+             "wa","wi","we","wo",
 
-program = input("Choose Program\n\n1. Write the Hiragana (Needs IME)\n2. Decipher the Monographs (Set length at start of file)\n> ")
+             #Diacritics
+             "ga","gi","gu","ge","go",
+             "za","ji","zu","ze","zo",
+             "da","ji","zu","de","do", 
+             "ba","bi","bu","be","bo",
+             "pa","pi","pu","pe","po"
+                                                        ]
+
+program = input("""Choose Program\n\n
+                            1. Write the Hiragana (Needs IME)\n
+                            2. Decipher the Monographs (Set length at start of file)\n> """)
 
 if program == "1":
     
@@ -63,7 +86,3 @@ if program == "2":
         else:
             print(english)
 
-
-
-
-    
